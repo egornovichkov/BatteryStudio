a3cc50a37eae3d42cdcbc727c43dd03bb2f8ed10
egornovichkov <novichkovegor41@gmail.com>
2024-10-05 15:03:42 +0300

    Basic functionality for cellsdatamodel added.

--- src/Models/CellsDataModel/cellsdatamodel.cpp
+++ src/Models/CellsDataModel/cellsdatamodel.cpp
@@ -3,3 +3,66 @@
 CellsDataModel::CellsDataModel(QObject *parent)
     : QAbstractTableModel{parent}
 {}
+
+int CellsDataModel::rowCount(const QModelIndex& parent) const
+{
+    Q_UNUSED(parent)
+    return m_cellsData.count();
+}
+
+int CellsDataModel::columnCount(const QModelIndex& parent) const
+{
+    Q_UNUSED(parent)
+    return CELLPARAMSCOUNT;
+}
+
+QVariant CellsDataModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role != Qt::DisplayRole)
+    {
+        return QVariant();
+    }
+
+    if (orientation == Qt::Vertical)
+    {
+        return section;
+    }
+
+    switch (section)
+    {
+        case Number:
+            return QString::fromUtf8("Number");
+        case Voltage:
+            return QString::fromUtf8("Voltage");
+
+    }
+
+    return QVariant();
+}
+
+QVariant CellsDataModel::data(const QModelIndex& index, int role) const
+{
+    if (
+        !index.isValid() ||
+        m_cellsData.count() <= index.row() ||
+        (role != Qt::DisplayRole && role != Qt::EditRole)
+    )
+    {
+        return QVariant();
+    }
+
+    return m_cellsData[index.row()][CellParams(index.column())];
+}
+
+bool CellsDataModel::setData(const QModelIndex& index, const QVariant& value, int role)
+{
+    if (!index.isValid() || role != Qt::EditRole || m_cellsData.count() <= index.row())
+    {
+        return false;
+    }
+
+    m_cellsData[index.row()][CellParams(index.column())] = value;
+    emit dataChanged(index, index);
+
+    return true;
+}
--- src/Models/CellsDataModel/cellsdatamodel.h
+++ src/Models/CellsDataModel/cellsdatamodel.h
@@ -5,8 +5,29 @@
 
 class CellsDataModel : public QAbstractTableModel
 {
+    Q_OBJECT
 public:
+    enum CellParams
+    {
+        Number = 0,
+        Voltage = 1,
+        CELLPARAMSCOUNT // Do not insert any params after this
+    };
+
     explicit CellsDataModel(QObject *parent = nullptr);
+
+    int rowCount(const QModelIndex& parent) const override;
+    int columnCount(const QModelIndex& parent) const override;
+    QVariant data(const QModelIndex& index, int role) const override;
+    bool setData(const QModelIndex& index, const QVariant& value, int role) override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+    // Qt::ItemFlags flags(const QModelIndex& index) const override;
+
+private:
+
+    typedef QHash<CellParams, QVariant> cellValues;
+    typedef QList<cellValues> cellsData;
+    cellsData m_cellsData;
 };
 
 #endif // CELLSDATAMODEL_H
