a966e56ba1e3ee5dce6de441fa5d709ca012dcfb
egornovichkov <novichkovegor41@gmail.com>
2024-10-16 13:42:34 +0300

    Added useful enums to BSTU namespace.

--- src/bstunamespace.h
+++ src/bstunamespace.h
@@ -1,18 +1,45 @@
 #ifndef BSTUNAMESPACE_H
 #define BSTUNAMESPACE_H
 
+#include "qnamespace.h"
 namespace BSTU
 {
+
+    /// Enumeration for cells model type.
+    enum CellsModel
+    {
+        VoltageType = 0,
+        TemperatureType = 1
+    };
+
     /// Enumeration for cell parameters names.
     enum CellParams
     {
         // Params must be equal to int numbers from 0 to parameters count in order
         Number = 0,
         Voltage = 1,
-        Minimum = 2,
-        Maximum = 3,
-        Color = 4,
+        Temperature = 2,
         CELLPARAMSCOUNT // Do not insert any params after this
+    };
+
+    /// Enumeration for roles used by models.
+    enum CellRoles
+    {
+        NumRole = Qt::UserRole,
+        VoltRole,
+        TempRole,
+        MinVoltRole,
+        MaxVoltRole,
+        MinTempRole,
+        MaxTempRole,
+        VoltRangeRole,
+        TempRangeRole,
+        ModelTypeRole
+    };
+
+    enum CellsTableRoles
+    {
+        RelativeBarHeightRole = 2 * Qt::UserRole
     };
 }
 
