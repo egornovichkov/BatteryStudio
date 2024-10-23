b36a49183cb064323191f815c264f94010700a71
egornovichkov <novichkovegor41@gmail.com>
2024-10-23 18:18:31 +0300

    Hided unused cells.

--- src/Views/CellsDataView/cellstableviewwidget.cpp
+++ src/Views/CellsDataView/cellstableviewwidget.cpp
@@ -1,35 +1,60 @@
 #include "cellstableviewwidget.h"
 #include "cellsviewdelegate.h"
+#include "src/bstunamespace.h"
 #include <QPainter>
 #include <QHeaderView>
+#include <iostream>
+
+// Re-implementation of QHeaderView
+// class MyHeaderView : public QHeaderView {
+// public:
+//     MyHeaderView(QWidget* parent = NULL) : QHeaderView(Qt::Vertical,parent){}
+//     virtual void resizeEvent(QResizeEvent * event);
+// };
+
+// void MyHeaderView::resizeEvent(QResizeEvent * event) {
+//     QHeaderView::resizeEvent(event);
+//     emit geometriesChanged();
+// }
 
 CellsTableViewWidget::CellsTableViewWidget(QWidget *parent)
 {
     Q_UNUSED(parent);
     CellsViewDelegate *delegate = new CellsViewDelegate(this);
     setItemDelegate(delegate);
-    horizontalHeader()->hide();
-    verticalHeader()->hide();
+    // MyHeaderView* header = new MyHeaderView(this);
+    // setVerticalHeader(header);
     verticalHeader()->setSectionResizeMode(QHeaderView::Stretch);
     horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
     verticalHeader()->setMinimumSectionSize(30);
     horizontalHeader()->setMinimumSectionSize(43);
+    horizontalHeader()->hide();
+    verticalHeader()->hide();
 }
 
 void CellsTableViewWidget::paintEvent(QPaintEvent *e)
 {
-    // QPainter painter(this);
-
     QTableView::paintEvent(e);
 
-    // int lastCellCol = model()->data(QModelIndex(), BSTU::LastCellIndexRole).toInt();
-    // int lastCellRow = model()->rowCount();
-    // int x = columnViewportPosition(lastCellCol);
-    // int y = rowViewportPosition(lastCellRow);
-    // painter.setBrush(Qt::gray);
-    // painter.drawRect(x, y, 20, 20);
+    QPainter painter(viewport());
+    // painter.drawRect(0,0,height(),width());
+
+
+    if (model())
+    {
+        int lastCellCol = model()->data(QModelIndex(), BSTU::LastCellIndexRole).toInt();
+        int lastCellRow = model()->rowCount() - 1;
+        std::cout << columnViewportPosition(lastCellCol) << "\n";
+        std::cout << rowViewportPosition(lastCellRow) << "\n";
+        int x = columnViewportPosition(lastCellCol);
+        int y = rowViewportPosition(lastCellRow);
+        painter.setBrush(QColor(244, 244, 249));
+        painter.setPen(QColor(244, 244, 249));
+        painter.drawRect(x, y, width() - x, height() - y);
+    }
     // Hiding unused cells
     // ...................
 
     // Calling parent's paintEvent to draw default QTableWidget
+
 }
